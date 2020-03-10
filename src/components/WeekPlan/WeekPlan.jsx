62e6291bc5ccf8b78ff3285066b042fe9e84b2b3
Tony Chen <anacrusy.tc@gmail.com>
2020-03-10 11:08:55 -0400

    Edited week plan so popovers work for Tuesday

--- src/components/WeekPlan/WeekPlan.jsx
+++ src/components/WeekPlan/WeekPlan.jsx
@@ -7,15 +7,21 @@ export default class WeekPlan extends Component {
   constructor() {
     super();
     this.state = {
-      visible: false,
-			temp: []
+      visible: false, 
+	  visible2: false,
+			temp: [],
+			temp2: []
     };
   }
 	
-	  handleVisibleChange = visible => {
+	  handleVisibleChange = visible=> {
     this.setState({ visible });
+	
   };
-
+	handleVisibleChange2 = visible2 => {
+    this.setState({ visible2 });
+	};
+	
   render() {
     return (
       <div className="WeekPlan-Wrapper">
@@ -29,51 +35,74 @@ export default class WeekPlan extends Component {
                   </Card>
                 ))}
             </div>
-						      <Popover
-										placement="right"
-        					content={this.props.user_favourites.map(recipe => (
-                    <p key={recipe.id} onClick={() => this.setState({temp: [recipe], visible: false})} style={{ cursor: "pointer"}}>{recipe.title}</p>
-                		))}
+				<Popover placement="right" content={this.props.user_favourites.map(recipe => (
+                  <p key={recipe.id} onClick={() => this.setState({temp: [recipe], visible: false})} style={{ cursor: "pointer"}}>{recipe.title}</p>
+                ))}
         title="Choose a meal to add"
         visible={this.state.visible}
-        onVisibleChange={this.handleVisibleChange}
-      >
+        onVisibleChange={this.handleVisibleChange}>
+		
             <PlusCircleOutlined
               key="plus"
               onClick={() => this.setState({ visible: true })}
             />
-						</Popover>
-          </div>
+						</Popover>        
+		  </div>
+		  
           <div className="column">
             Monday
             <div />
             <PlusCircleOutlined key="plus" />
           </div>
+		  
           <div className="column">
             Tuesday
-            <div />
-            <PlusCircleOutlined key="plus" />
+			<div>
+              {this.state.temp2.map(recipe => (
+                  <Card key={recipe.id} style={{ marginBottom: "10px" }}>
+                    <p>{recipe.title}</p>
+                  </Card>
+                ))}
+            </div>
+						      <Popover
+										placement="right"
+        					content={this.props.user_favourites.map(recipe => (
+                    <p key={recipe.id} onClick={() => this.setState({temp2: [recipe], visible2: false})} style={{ cursor: "pointer"}}>{recipe.title}</p>
+                		))}
+        title="Choose a meal to add"
+        visible2={this.state.visible2}
+        onVisibleChange={this.handleVisibleChange2}>
+            <PlusCircleOutlined
+              key="plus"
+              onClick={() => this.setState({ visible2: true })}
+            />
+						</Popover>
           </div>
+		  
           <div className="column">
             Wednesday
             <div />
             <PlusCircleOutlined key="plus" />
           </div>
+		  
           <div className="column">
-            Thursday>
+            Thursday
             <div />
             <PlusCircleOutlined key="plus" />
           </div>
+		  
           <div className="column">
             Friday
             <div />
             <PlusCircleOutlined key="plus" />
           </div>
+		  
           <div className="column">
             Saturday
             <div />
             <PlusCircleOutlined key="plus" />
           </div>
+		  
         </div>
       </div>
     );
