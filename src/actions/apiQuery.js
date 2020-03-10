62801c33931d7ef0f47b386654aaa2c7bc8dfa74
Tyler Elliott <tdselliott@gmail.com>
2020-03-10 11:29:10 -0400

    Switch API key

--- src/actions/apiQuery.js
+++ src/actions/apiQuery.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 const searchRecipe = (text) => {
-  return axios.get(`https://api.spoonacular.com/recipes/search?query=${text}&number=5&apiKey=0cbf95a570fb46dab0b6ff718035d0e1`)
+  return axios.get(`https://api.spoonacular.com/recipes/search?query=${text}&number=5&apiKey=06685c5e1a0c42598d09bfc358d5e5a1`)
 }
 
 const searchAdvanced = (text, diet) => {
-  return axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${text}&number=5&diet=${diet}&apiKey=0cbf95a570fb46dab0b6ff718035d0e1`)
+  return axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${text}&number=5&diet=${diet}&apiKey=06685c5e1a0c42598d09bfc358d5e5a1`)
 }
 
 const getIngredients = (text) => {
-  return axios.get(`https://api.spoonacular.com/recipes/${text}/information?includedNutrition=false&apiKey=0cbf95a570fb46dab0b6ff718035d0e1`)
+  return axios.get(`https://api.spoonacular.com/recipes/${text}/information?includedNutrition=false&apiKey=06685c5e1a0c42598d09bfc358d5e5a1`)
 }
 
 const getRandomRecipes = () => {
-  return axios.get(`https://api.spoonacular.com/recipes/random?number=5&apiKey=0cbf95a570fb46dab0b6ff718035d0e1`);
+  return axios.get(`https://api.spoonacular.com/recipes/random?number=5&apiKey=06685c5e1a0c42598d09bfc358d5e5a1`);
 }
 
 export {
